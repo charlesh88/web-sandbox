73f06a50df880862c0b48cc4bc0cc405248051b9
Charles <charlesh88@gmail.com>
2013-04-11 00:31:06 -0700

    Static markup and CSS for VFN NFL Draft feature story

---
+++ nfl-draft/index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<head>
+
+<link href="static/css/screen.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="app-widget app-widget-feature-story">
+    <!-- actual component -->
+    <div class="widget-wrapper">
+      <div class="content-wrapper">
+        <h1 class="headline">Behind the scenes <span class="em">2013 NFL Draft</span></h1>
+        <h2 class="sub-headline">Follow all of the action - exclusive draft videos published by professional athletes</h2>
+        <h2 class="sub-headline em">Private beta launch - May 14</h2>
+        <div class="cta">
+          Are you an athlete? <a href="" class="btn">Join the Sqor Team<img class="arw" src="static/images/arw-sm-right.png" /></a>
+        </div>
+      </div>
+      <img class="bg" src="static/images/featured-nfl-draft-2013.jpg" />
+    </div>
+    <!-- / actual component -->
+  </div>
+</body>
+
+</html>
